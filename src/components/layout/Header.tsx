ae77d1c68fedb692af7736d8a60d7f5960a14b93
Brian Nguyen <nhgbao2001089@student.ctuet.edu.vn>
2025-04-07 00:02:01 +0700

    update

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,12 @@
 'use client'
 
-import { useEffect, useLayoutEffect, useRef, useState } from 'react'
-import { usePathname } from 'next/navigation'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import {
+	useRef,
+	// useEffect, useLayoutEffect, useState
+} from 'react'
+// import { usePathname } from 'next/navigation'
+// import gsap from 'gsap'
+// import { ScrollTrigger } from 'gsap/ScrollTrigger'
 // import { AnimatePresence, motion } from 'framer-motion'
 
 import { Logo } from '~/components/ui/Logo'
@@ -15,80 +18,80 @@ import { Logo } from '~/components/ui/Logo'
 
 export const Header = () => {
 	const headerRef = useRef<HTMLDivElement | null>(null)
-	const curvedNavButtonRef = useRef<HTMLDivElement | null>(null)
-	const curvedNavRef = useRef<HTMLDivElement | null>(null)
+	// const curvedNavButtonRef = useRef<HTMLDivElement | null>(null)
+	// const curvedNavRef = useRef<HTMLDivElement | null>(null)
 
-	const [isCurvedNavActive, setIsCurvedNavActive] = useState<boolean>(false)
-	const [isSideMenuActive, setIsSideMenuActive] = useState<boolean>(false)
+	// const [isCurvedNavActive, setIsCurvedNavActive] = useState<boolean>(false)
+	// const [isSideMenuActive, setIsSideMenuActive] = useState<boolean>(false)
 
-	const pathname = usePathname()
+	// const pathname = usePathname()
 
-	useEffect(() => {
-		const handleClickOutside = (event: MouseEvent) => {
-			if (
-				curvedNavRef.current &&
-				!curvedNavRef.current.contains(event.target as Node) &&
-				curvedNavButtonRef.current &&
-				!curvedNavButtonRef.current.contains(event.target as Node)
-			) {
-				setIsCurvedNavActive(false)
-			}
-		}
+	// useEffect(() => {
+	// 	const handleClickOutside = (event: MouseEvent) => {
+	// 		if (
+	// 			curvedNavRef.current &&
+	// 			!curvedNavRef.current.contains(event.target as Node) &&
+	// 			curvedNavButtonRef.current &&
+	// 			!curvedNavButtonRef.current.contains(event.target as Node)
+	// 		) {
+	// 			setIsCurvedNavActive(false)
+	// 		}
+	// 	}
 
-		if (isCurvedNavActive) {
-			document.addEventListener('mousedown', handleClickOutside)
-		}
+	// 	if (isCurvedNavActive) {
+	// 		document.addEventListener('mousedown', handleClickOutside)
+	// 	}
 
-		return () => {
-			document.removeEventListener('mousedown', handleClickOutside)
-		}
-	}, [isCurvedNavActive])
+	// 	return () => {
+	// 		document.removeEventListener('mousedown', handleClickOutside)
+	// 	}
+	// }, [isCurvedNavActive])
 
-	useEffect(() => {
-		if (isCurvedNavActive) setIsCurvedNavActive(false)
-		if (isSideMenuActive) setIsSideMenuActive(false)
-	}, [pathname])
+	// useEffect(() => {
+	// 	if (isCurvedNavActive) setIsCurvedNavActive(false)
+	// 	if (isSideMenuActive) setIsSideMenuActive(false)
+	// }, [pathname])
 
-	useEffect(() => {
-		const handleScroll = () => {
-			if (isSideMenuActive) {
-				setIsSideMenuActive(false)
-			}
-		}
+	// useEffect(() => {
+	// 	const handleScroll = () => {
+	// 		if (isSideMenuActive) {
+	// 			setIsSideMenuActive(false)
+	// 		}
+	// 	}
 
-		window.addEventListener('scroll', handleScroll)
-		return () => {
-			window.removeEventListener('scroll', handleScroll)
-		}
-	}, [isSideMenuActive])
+	// 	window.addEventListener('scroll', handleScroll)
+	// 	return () => {
+	// 		window.removeEventListener('scroll', handleScroll)
+	// 	}
+	// }, [isSideMenuActive])
 
-	useLayoutEffect(() => {
-		if (!curvedNavButtonRef.current) return
+	// useLayoutEffect(() => {
+	// 	if (!curvedNavButtonRef.current) return
 
-		gsap.registerPlugin(ScrollTrigger)
-		gsap.to(curvedNavButtonRef.current, {
-			scrollTrigger: {
-				trigger: document.documentElement,
-				start: 0,
-				end: window.innerHeight,
-				onLeave: () => {
-					gsap.to(curvedNavButtonRef.current, {
-						scale: 1,
-						duration: 0.25,
-						ease: 'power1.out',
-					})
-				},
-				onEnterBack: () => {
-					gsap.to(curvedNavButtonRef.current, {
-						scale: 0,
-						duration: 0.25,
-						ease: 'power1.out',
-						onComplete: () => setIsCurvedNavActive(false),
-					})
-				},
-			},
-		})
-	}, [])
+	// 	gsap.registerPlugin(ScrollTrigger)
+	// 	gsap.to(curvedNavButtonRef.current, {
+	// 		scrollTrigger: {
+	// 			trigger: document.documentElement,
+	// 			start: 0,
+	// 			end: window.innerHeight,
+	// 			onLeave: () => {
+	// 				gsap.to(curvedNavButtonRef.current, {
+	// 					scale: 1,
+	// 					duration: 0.25,
+	// 					ease: 'power1.out',
+	// 				})
+	// 			},
+	// 			onEnterBack: () => {
+	// 				gsap.to(curvedNavButtonRef.current, {
+	// 					scale: 0,
+	// 					duration: 0.25,
+	// 					ease: 'power1.out',
+	// 					onComplete: () => setIsCurvedNavActive(false),
+	// 				})
+	// 			},
+	// 		},
+	// 	})
+	// }, [])
 
 	return (
 		<>
